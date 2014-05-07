43d5b2fb0343ea64d92a250c2287d4bee2d6c2d4
ncgVenerat <nviiicg@gmail.com>
2014-05-07 10:46:58 -0400

    Re-upload
    Re-uploading all information

---
+++ js/js-main.js
@@ -0,0 +1,5 @@
+function jmp2LocalPage(whichPage) {
+	location.href = whichPage;
+}
+
+var data = [4, 8, 15, 16, 23, 42];
---
+++ php/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width, maximum-scale=1.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/css-normalize.css">
+        <link rel="stylesheet" href="css/css-add2home.css">
+        <link rel="stylesheet" href="css/css-main.css">
+        <!-- iPhone ICON -->
+        <link rel="apple-touch-icon" href="img/placeholder_icon-57x57.png" sizes="57x57">
+        <link rel="apple-touch-icon" href="img/placeholder_icon-114x114.png" sizes="114x114">
+        <!-- iPhone splashscreens-->
+        <link rel="apple-touch-startup-image" href="img/markersplash.png" sizes="320x460" media="(device-width:320px)">
+        <link rel="apple-touch-startup-image" href="img/markersplash-larger.png" sizes="640x920" media="(device-width:320px) and (-webkit-device-pixel-ratio: 2">
+    </head>
+    <title><!--GET name from WCLevel1.html click--></title>
+    </head>
